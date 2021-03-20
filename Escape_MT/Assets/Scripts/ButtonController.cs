91f55124cc2f66d85cf6c2a5cf84c8d1bac812d4
martin-newbie <g.martin.min@gmail.com>
2021-03-20 15:44:51 +0900

    complete title frame

---
+++ Escape_MT/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject optionWindow;
+    [SerializeField] private GameObject manual;
+
+    private bool optionActive;
+    private bool manualActive;
+
+    void Start()
+    {
+        optionActive = false;
+        manualActive = false;
+    }
+
+    void Update()
+    {
+        optionWindow.SetActive(optionActive);
+        manual.SetActive(manualActive);
+    }
+
+    public void OptionController()
+    {
+        optionActive = !optionActive;
+    }
+
+    public void ManualController()
+    {
+        manualActive = !manualActive;
+    }
+}
---
+++ Escape_MT/Assets/Scripts/SceneController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ToIngameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ToTitleScene()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    public void ToMinigameScene()
+    {
+
+    }
+
+    public void ToGameoverScene()
+    {
+
+    }
+}
